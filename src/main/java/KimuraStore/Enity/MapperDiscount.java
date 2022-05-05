8f1a40841a4f2df2903ff74211eb664790fa9195
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2022-05-05 23:30:44 +0700

    add discount product data

---
+++ src/main/java/KimuraStore/Enity/MapperDiscount.java
@@ -0,0 +1,18 @@
+package KimuraStore.Enity;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MapperDiscount implements RowMapper<Discount> {
+
+    @Override
+    public Discount mapRow(ResultSet resultSet, int i) throws SQLException {
+        Discount discount = new Discount();
+        discount.setId(resultSet.getInt("id"));
+        discount.setDiscount_money(resultSet.getDouble("discount_money"));
+        discount.setDiscount_rate(resultSet.getDouble("discount_rate"));
+        return discount;
+    }
+}
