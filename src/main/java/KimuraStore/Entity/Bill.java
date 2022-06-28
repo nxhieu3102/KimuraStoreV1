2e11327c39bae502c5b849a15f59b61cf5123999
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2022-06-28 21:16:22 +0700

    add bill

---
+++ src/main/java/KimuraStore/Entity/Bill.java
@@ -0,0 +1,71 @@
+package KimuraStore.Entity;
+
+import java.util.Date;
+
+public class Bill {
+    public int id;
+    public int userId;
+    public String phone;
+    public String address;
+    public double totalPrice;
+    public String name;
+    public Date checkoutDate;
+
+    public Bill(){}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCheckoutDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(Date checkoutDate) {
+        this.checkoutDate = checkoutDate;
+    }
+}
---
+++ src/main/java/KimuraStore/Entity/Mapper/MapperBill.java
@@ -0,0 +1,22 @@
+package KimuraStore.Entity.Mapper;
+
+import KimuraStore.Entity.Bill;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MapperBill implements RowMapper<Bill> {
+    @Override
+    public Bill mapRow(ResultSet resultSet, int i) throws SQLException {
+        Bill bill = new Bill();
+        bill.setId(resultSet.getInt("id"));
+        bill.setUserId(resultSet.getInt("user_id"));
+        bill.setPhone(resultSet.getString("phone"));
+        bill.setAddress(resultSet.getString("address"));
+        bill.setTotalPrice(resultSet.getDouble("total_price"));
+        bill.setName(resultSet.getString("name"));
+
+        return bill;
+    }
+}
